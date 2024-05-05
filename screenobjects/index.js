ad2001630aebbfaaabbed679b3ba7c2b1951656a
Vilius Jurgelevičius <vilius.jurgelevicius@voltasit.com>
2024-05-05 14:11:14 +0300

    Added comments for index.js

--- screenobjects/index.js
+++ screenobjects/index.js
@@ -1,4 +1,11 @@
+// Import any available screenobject modules
 const LandingPage = require('./landing.page');
 const CartPage = require('./cart.page');
 
+/**
+ * Module exporting page objects
+ * @module PageObjects
+ */
+
+// Export the screenobject modules for use
 module.exports = { LandingPage, CartPage };
